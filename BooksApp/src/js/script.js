8ec44c5fae344c29b8ee37381c32ac9963dfa2c4
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-08-24 19:34:49 +0200

    Update script.js

--- BooksApp/src/js/script.js
+++ BooksApp/src/js/script.js
@@ -1,27 +1,28 @@
-'use strict'
-
+'use strict';
 const select = {
-    templateOf: {
-        books: '#template-book',
-    },
-    listOf: {
-        bookList: '.book-list',
-    },
-}
+  templateOf: {
+    books: '#template-book',
+  },
+  listOf: {
+    booksList: '.books-list',
+  },
+};
 
 const templates = {
-    bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML),
-  };
+  bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.books).innerHTML),
+};
 
 function render() {
 
-    for(let book of dataSource.books){
-        const generatedHTML = templates.bookTemplate(book);
+  for(let book of dataSource.books){
+    const generatedHTML = templates.bookTemplate(book);
 
-        thisBookDOMElement = utils.createDOMFromHTML(generatedHTML);
+    const thisBookDOMElement = utils.createDOMFromHTML(generatedHTML);
 
-        const bookListContainer = document.querySelector(select.listOf.bookList);
+    const bookListContainer = document.querySelector(select.listOf.booksList);
 
-        bookListContainer.appendChild(thisBookDOMElement);
-    }
+    bookListContainer.appendChild(thisBookDOMElement);
+  }
 }
+
+render();
