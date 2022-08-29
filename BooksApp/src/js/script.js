1c3cbf363bc3fe43985b7c5dee78f6e6ffe46b13
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-08-29 15:10:01 +0200

    Update script.js

--- BooksApp/src/js/script.js
+++ BooksApp/src/js/script.js
@@ -85,7 +85,7 @@ function initActions() {
 }
 
 function filterBooks(){
-const bookImageID = document.querySelector(select.imageParams.id);
+const bookImageID = document.querySelector(select.listOf.image + '[data-id: "' + imageParams.id + '"]');
 for(const book of dataSource.books){
     let shouldBeHidden = false;
     for(const filter of filters){
