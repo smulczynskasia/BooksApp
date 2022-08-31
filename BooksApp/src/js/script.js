149f693377e35681f046940575400f1a9e2eb77a
smulczynskasia <107355316+smulczynskasia@users.noreply.github.com>
2022-08-31 17:54:43 +0200

    Update script.js

--- BooksApp/src/js/script.js
+++ BooksApp/src/js/script.js
@@ -26,6 +26,8 @@ function render() {
     const bookListContainer = document.querySelector(select.listOf.booksList);
 
     bookListContainer.appendChild(thisBookDOMElement);
+    const ratingBgc = this.determineRatingBgc(book.rating);
+    const ratingWidth = book.rating * 10;
   }
 }
 
@@ -102,6 +104,18 @@ for(const book of dataSource.books){
 
 }
 
+}
+
+function   determineRatingBgc(rating) {
+    if (rating < 6) {
+      return 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%);';
+    } else if (rating > 6 && rating <= 8) {
+      return 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%);';
+    } else if (rating > 8 && rating <= 9) {
+      return 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%);';
+    } else if (rating > 9) {
+      return 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%);';
+    }
 }
 
 render();
